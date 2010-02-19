2ea34fb4c2a36ef31976ea6cbb95fec50e9d5a4d
Nils Hörrmann <post@nilshoerrmann.de>
2010-02-19 14:52:00 +0100

    First concept

--- symphony.stage.js
+++ symphony.stage.js
@@ -1,26 +1,76 @@
-/**
- * STAGE for Symphony
- *
- * @author: Nils Hörrmann, post@nilshoerrmann.de
- * @date: January 2010
- */
-
-
+/*-----------------------------------------------------------------------------
+	Language strings
+-----------------------------------------------------------------------------*/
+ 
+	Symphony.Language.add({
+		'Add item': false,
+		'Remove item': false
+	});
+ 
+/*-----------------------------------------------------------------------------
+	Stage plugin
+-----------------------------------------------------------------------------*/
+	
 	jQuery.fn.symphonyStage = function(custom_settings) {
-
 		var objects = this;
 		var settings = {
-			dublicatable = true,
-			draggable = true,
-			sortable = true
-			queue = false,
-			search = false
+			instances:			'> li:not(.template)',	// What children do we use as instances?
+			templates:			'> li.template',		// What children do we use as templates?
+			headers:			'> :first-child',		// What part of an instance is the header?
+			orderable:			true,					// Can instances be ordered?
+			collapsible:		true,					// Can instances be collapsed?
+			constructable:		true,					// Allow construction of new instances?
+			destructable:		true,					// Allow destruction of instances?
+			minimum:			0,						// Do not allow instances to be removed below this limit.
+			maximum:			1000,					// Do not allow instances to be added above this limit.
+			speed:				'fast',					// Control the speed of any animations
+			delay_initialize:	false
 		};
- 
+			
 		jQuery.extend(settings, custom_settings);
-
-		return objects.each(function() {
 		
+	/*-------------------------------------------------------------------------
+		Collapsible
+	-------------------------------------------------------------------------*/
+		
+		if (settings.collapsible) objects = objects.symphonyCollapsible({
+			items:			'.instance',
+			handles:		'.handle'
 		});
+		
+	/*-------------------------------------------------------------------------
+		Orderable
+	-------------------------------------------------------------------------*/
+		
+		if (settings.orderable) objects = objects.symphonyOrderable({
+			items:			'.instance',
+			handles:		'.handle'
+		});
+		
+	/*-------------------------------------------------------------------------
+		Stage
+	-------------------------------------------------------------------------*/
+		
+		objects = objects.map(function() {
+			var object = this;
+			
+		/*-------------------------------------------------------------------*/
+			
+			if (object instanceof jQuery === false) {
+				object = jQuery(object);
+			}
+			
+			object.stage = {
 
+			};
+			
+			if (settings.delay_initialize !== true) {
+				//object.stage.initialize();
+			}
+			
+			return object;
+		});
+		
+		return objects;
 	};
+	
